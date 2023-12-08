e3853430b13d3ddda41e7f1aef94d7af55b3d9e3
Basheer Becerra <bbecerr@outlook.com>
2023-12-08 20:03:55 +0800

    Replaced input DF for encoding matrix with the padded DF

--- crispr-ambiguous-mapping/crispr_ambiguous_mapping/mapping/guide_counting.py
+++ crispr-ambiguous-mapping/crispr_ambiguous_mapping/mapping/guide_counting.py
@@ -184,11 +184,11 @@ def get_whitelist_reporter_counts_with_umi(observed_guide_reporter_umi_counts: D
     #
     # ENCODE THE WHITELISTED SEQUENCES INTO NUMPY MATRICES - THIS IS REQUIRED FOR HAMMING-BASED MAPPING
     #
-    encoded_whitelist_protospacer_sequences_series = sequence_encoding.encode_guide_series(whitelist_guide_reporter_df["protospacer"])
+    encoded_whitelist_protospacer_sequences_series = sequence_encoding.encode_guide_series(padded_whitelist_guide_reporter_df["protospacer"])
     if contains_surrogate:
-        encoded_whitelist_surrogate_sequences_series = sequence_encoding.encode_guide_series(whitelist_guide_reporter_df["surrogate"])
+        encoded_whitelist_surrogate_sequences_series = sequence_encoding.encode_guide_series(padded_whitelist_guide_reporter_df["surrogate"])
     if contains_barcode:
-        encoded_whitelist_barcode_sequences_series = sequence_encoding.encode_guide_series(whitelist_guide_reporter_df["barcode"])
+        encoded_whitelist_barcode_sequences_series = sequence_encoding.encode_guide_series(padded_whitelist_guide_reporter_df["barcode"])
 
     
 
