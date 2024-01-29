250bafb4699777fb993da87b5935856fbf64c9b3
Basheer Becerra <bbecerr@outlook.com>
2024-01-29 15:16:23 -0500

    Fix function to calc editing efficiency where there are no reads

--- crispr-ambiguous-mapping/crispr_ambiguous_mapping/utility/crispr_editing_utility.py
+++ crispr-ambiguous-mapping/crispr_ambiguous_mapping/utility/crispr_editing_utility.py
@@ -1,5 +1,10 @@
 from typing import Counter as CounterType
 
 def calculate_average_editing_frequency(total_mutation_counter: CounterType) -> float:
-    editing_efficiency = (sum(total_mutation_counter.values()) - total_mutation_counter[0])/sum(total_mutation_counter.values())
-    return editing_efficiency
+    total_editing_counts = (sum(total_mutation_counter.values()) - total_mutation_counter[0])
+    total_counts = sum(total_mutation_counter.values())
+    if total_counts > 0:
+        editing_efficiency = total_editing_counts/total_counts
+        return editing_efficiency
+    else:
+        return 0
